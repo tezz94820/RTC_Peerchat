a59be27a7aa14af7c03ce7a87cc7acf8cdd22f7e
Tejas Itankar <tejasitankar94820@gmail.com>
2023-06-11 16:42:01 +0530

    completed the project

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ let userId = Math.floor(Math.random()*10000).toString()   // this is the unique 
 let client; //client who is using the agora server
 let channel;    // the communication link or tunnel where both the users can talk
 
-
+//room query
+let queryString = window.location.search
+let urlParams = new URLSearchParams(queryString)
+let roomId = urlParams.get('room')
 
 let localStream;
 let remoteStream;
@@ -21,6 +24,15 @@ const servers = {
     ]
 }
 
+const constraints = {
+    video:{
+        width:{min:640,ideal:1920,max:1920},
+        height:{min:480,ideal:1080,max:1080}
+    },
+    audio:true
+}
+
+
 const init = async () => {
     //creating the Agora instance in client and login in that client using its userID
     client = await AgoraRTM.createInstance(APP_ID)
@@ -28,7 +40,7 @@ const init = async () => {
     console.log("my MemberID"+userId)
 
     //index.html?room=12345 , the main can be replaced by the roomID later
-    channel =  client.createChannel('main')
+    channel =  client.createChannel(roomId)
     await channel.join()
 
     //if any other user is joined then we can listen to that event using MemberJoined event
@@ -40,7 +52,7 @@ const init = async () => {
     client.on('MessageFromPeer', handleMessageFromPeer)
 
     //getting the access to user media devices
-    localStream = await navigator.mediaDevices.getUserMedia({video: true,audio: false});
+    localStream = await navigator.mediaDevices.getUserMedia(constraints);
     //assigning it to user-1 video player
     document.getElementById('user-1').srcObject = localStream;
 }
@@ -53,6 +65,7 @@ let handleUserJoined = async (MemberId) => {
 
 let handleUserLeft = async (MemberId) => {
     document.getElementById('user-2').style.display = 'none';
+    document.getElementById('user-2').classList.remove('smallFrame');
 }
 
 let handleMessageFromPeer = async (message,MemberId) => {
@@ -79,9 +92,10 @@ const createPeerConnection = async (MemberId) => {
     remoteStream = new MediaStream()
     document.getElementById('user-2').srcObject = remoteStream;
     document.getElementById('user-2').style.display = 'block';
+    document.getElementById('user-2').classList.add('smallFrame')
 
     if(!localStream) {//if local stream is not available then create it 
-        localStream = await navigator.mediaDevices.getUserMedia({video: true,audio: false});
+        localStream = await navigator.mediaDevices.getUserMedia(constraints);
         document.getElementById('user-1').srcObject = localStream;
     }
 
@@ -146,6 +160,43 @@ let leaveChannel = async () => {
     await client.logout()
 }
 
+//to on and off camera connections
+let toggleCamera = async () => {
+    //get the video tracks
+    let videoTrack = localStream.getTracks().find( track => track.kind === 'video')
+
+    //if video track is enabled make it unenabled
+    if(videoTrack.enabled) {
+        videoTrack.enabled = false
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(255,80,80)'
+    }
+    else{//if it is off then make it on 
+        videoTrack.enabled = true
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(179,102,249,0.9)'
+    }
+}
+
+//to on and off mic connections
+let toggleAudio = async () => {
+    //get the video tracks
+    let audioTrack = localStream.getTracks().find( track => track.kind === 'audio')
+
+    //if video track is enabled make it unenabled
+    if(audioTrack.enabled) {
+        audioTrack.enabled = false
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(255,80,80)'
+    }
+    else{//if it is off then make it on 
+        audioTrack.enabled = true
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(179,102,249,0.9)'
+    }
+}
+
+//listening to leavechannel event
 window.addEventListener('beforeunload', leaveChannel)
+//listening to camera btn click event 
+document.getElementById('camera-btn').addEventListener('click',toggleCamera)
+//listening to mic btn click event 
+document.getElementById('mic-btn').addEventListener('click',toggleAudio)
 
 init();
